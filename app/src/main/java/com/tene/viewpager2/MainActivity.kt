054e48a111a3fe8e8749b4c8eafd199761fab4fd
avsheksingh <avsheksingh@outlook.com>
2019-04-10 10:27:46 +0530

    viewpager2 added

--- app/src/main/java/com/tene/viewpager2/MainActivity.kt
+++ app/src/main/java/com/tene/viewpager2/MainActivity.kt
@@ -2,11 +2,14 @@ package com.tene.viewpager2
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewPager2.adapter = ViewPagerAdapter()
     }
 }
